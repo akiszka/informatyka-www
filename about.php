d412b67548fcf65339b41077d3deca9740589f53
Antoni Kiszka <antonikiszka@icloud.com>
2020-06-05 18:03:17 +0200

    Finished About Us page.

--- about.php
+++ about.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="utf-8">
 <link rel="stylesheet" href="res/css/index.css">
+<link rel="stylesheet" href="res/css/aboutus.css">
 </head>
 <body>
 
@@ -22,10 +23,54 @@
     </header>
 
     <article id="change-your-future">
-        <h1>Zmień swoją przyszłość z nami!</h1>
-        <p>Zgodnie z naszymi statystykami, 90% ludzi nigdy nie odkrywa swojego pełnego potencjału.
-        Postawiliśmy sobie za cel zmianę tego smutnego stanu rzeczy.
-        Dołącz do nas i wspólnie zmieńmy świat!</p>
+        <div class="container">
+            <h1>Zmień swoją przyszłość z nami!</h1>
+            <p>Zgodnie z naszymi statystykami, 90% ludzi nigdy nie odkrywa swojego pełnego potencjału.
+            Postawiliśmy sobie za cel zmianę tego smutnego stanu rzeczy.
+            Dołącz do nas i wspólnie zmieńmy świat! Zaufały nam już tysiące osób. Oto niektóre z nich:</p>
+
+            <section id="gallery">
+                <div class="pic-container">
+                    <img class="person-pic" src="res/img/person1.jpg" alt="zadowolony klient">
+                    <div class="pic-overlay">Beata Pawlikowska - wyszła z niemocy i depresji, ty też możesz 
+                        (<a href="https://lubimyczytac.pl/ksiazka/3780923/wyszlam-z-niemocy-i-depresji-ty-tez-mozesz" target="_blank">nawet napisała o tym książkę</a>)</div>
+                </div>
+
+                <div class="pic-container">
+                    <img class="person-pic" src="res/img/person2.jpg" alt="zadowolony klient">
+                    <div class="pic-overlay">Ryszard Terlecki - poseł na Sejm RP, kiedyś hipis i narkoman, dzięki nam odmłodniał 40 lat</div>
+                </div>
+
+                <div class="pic-container">
+                    <img class="person-pic" src="res/img/person3.jpg" alt="zadowolony klient">
+                    <div class="pic-overlay">Alicja Flak - jej rodzina nie mogła zaakceptować, że nie lubi grzybów</div>
+                </div>
+
+                <div class="pic-container">
+                    <img class="person-pic" src="res/img/person4.jpg" alt="zadowolony klient">
+                    <div class="pic-overlay">Grzegorz Kowalski - z naszą pomocą wydostał się z biedy i jest milionerem</div>
+                </div>
+            </section>
+
+            <h1>Jak to dokładnie działa?</h1>
+            <p>Nasz sposób działania jest prosty. Dzięki pomocy ze storny Ryszarda Terleckiego, uzyskaliśmy dostęp do bazy PESEL, więc twoje dane już tutaj są!
+                Wystarczy, że się zalogujesz i przejdziesz do naszego formularza.
+            </p>
+            <p>Gdy tylko dopełnisz formalności, będziemy gotowi odmienić Twoje życie. Zgodnie z naszym Regulaminem, zmienimy Cię w lepszego człowieka w maksymalnie 3 dni.</p>
+            <h1>Nota prawna</h1>
+            <p>Zaakceptowanie Regulaminu wiąże się ze stałym i nieodwołalym zrzeknięciem się praw człowieka, w szczególności prawa do nietykalności cielesnej.
+                Reprezentant Antoni Kiszka Enterprises ma prawo zgłosić się do Klienta (lub członka jego rodziny oddalonego o maksymalnie 3 pokolenia) w dowolnej chwili jego życia
+                lub do 20 lat po śmierci i bezzwrotnie wypożyczyć dowolną ilość jego organów. 
+                Ponieważ prawo Rzeczypospolitej Polskiej nie pozwala na pełną egzekucję postanowień Regulaminu, jego podpisanie wiąże się z bezzwołcznym
+                zrzeknięciem się obywatelstwa polskiego.
+            </p>
+        </div>
+    </article>
+
+    <hr id="footer-line">
+
+    <article id="footer">
+        ANTONI KISZKA 1D
     </article>
 <body>
 </html>
